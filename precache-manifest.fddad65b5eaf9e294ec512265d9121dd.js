9217f147aadd001d0cd2670dca51d46a76d6e503
Anderson <anderson.hung@live.com>
2020-01-19 12:00:06 -0500

    Updates

---
+++ precache-manifest.fddad65b5eaf9e294ec512265d9121dd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fceea9e534dd847dac079cec018badc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35ffbbaa5c20c96fa14",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "54f3729e76e3cbea5661",
+    "url": "/static/css/main.ce27003c.chunk.css"
+  },
+  {
+    "revision": "c35ffbbaa5c20c96fa14",
+    "url": "/static/js/2.f4e7041a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f4e7041a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54f3729e76e3cbea5661",
+    "url": "/static/js/main.92f19849.chunk.js"
+  },
+  {
+    "revision": "1942d94238d4073766e2",
+    "url": "/static/js/runtime-main.ce7a4629.js"
+  },
+  {
+    "revision": "b30790ed18099ba9902496e2b4509261",
+    "url": "/static/media/Anderson_Resume.b30790ed.pdf"
+  },
+  {
+    "revision": "895ddea987172f5a34a727cb0b559c9d",
+    "url": "/static/media/Inter-Bold.895ddea9.woff"
+  },
+  {
+    "revision": "568245cc11b37221aa856cad876b519c",
+    "url": "/static/media/Inter-ExtraBold.568245cc.woff"
+  },
+  {
+    "revision": "8a12c702fa5c5e3ceed33f78e72b9bee",
+    "url": "/static/media/Inter-SemiBold.8a12c702.woff"
+  },
+  {
+    "revision": "20e0ab086aa768c6ec00c49c1c9861ff",
+    "url": "/static/media/TiemposHeadline-Regular.20e0ab08.otf"
+  },
+  {
+    "revision": "e27a812e01e31a737d44c564e16be5bd",
+    "url": "/static/media/arrow.e27a812e.svg"
+  },
+  {
+    "revision": "aa55d9d4077496983360c4791b31a51a",
+    "url": "/static/media/employ.aa55d9d4.png"
+  },
+  {
+    "revision": "719e10882568b6e781942904c237cad1",
+    "url": "/static/media/galaxy.719e1088.jpg"
+  },
+  {
+    "revision": "854d80a79fc76540702b77b41e7b26d9",
+    "url": "/static/media/jumbleberry.854d80a7.png"
+  },
+  {
+    "revision": "04b7068615b39f8100edfbc42a4f3c82",
+    "url": "/static/media/karma.04b70686.png"
+  },
+  {
+    "revision": "3562f7e7091f9f4504e58d3592d59ad6",
+    "url": "/static/media/profile.3562f7e7.jpeg"
+  }
+]);
